0f8d26bcc498ddfe91eefa2f0b7f449f4498a1b4
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-07-30 21:39:06 +0000

    Complete Task 6-B

--- Demo6Task_b.java
+++ Demo6Task_b.java
@@ -8,18 +8,21 @@ public class Demo6Task_b {
         int age2 = 30;
 
         String darkYellow = "\033[33m";
-        String blue= "\033[34m";
-        
-        String dashLine = "%s+-----------------+-----+\n";
-        String 
+        String blue= "\033[34;1m";
+        String reset = "\033[0m";
+        String white = "\033[37m";
+        String magenta = "\033[95m";
+
+        String DASHLINE = "%s+-----------------+-----+\n";
+        final String RECORD  = "| %3$s%-16s%4$s|%5$s%4s%4$s |\n";
 
 
-        System.out.printf(dashLine, darkYellow);
-        System.out.printf("| %3$s%-16s%4$s| %3$s%s%4$s |\n", "NAME", "AGE", blue, darkYellow);
-        System.out.printf(dashLine, darkYellow);
-        System.out.printf("| %3$s%-16s%4$s|%3$s%4s%4$s |\n", name1, age1, blue, darkYellow);
-        System.out.printf("| %3$s%-16s%4$s|%3$s%4s%4$s |\n", name2, age2, blue, darkYellow);
-        System.out.printf(dashLine, darkYellow);
+        System.out.printf(DASHLINE, darkYellow);
+        System.out.printf("| %3$s%-16s%5$s%4$s| %3$s%s%5$s%4$s |\n", "NAME", "AGE", blue, darkYellow, reset);
+        System.out.printf(DASHLINE, darkYellow);
+        System.out.printf(RECORD, name1, age1, white, darkYellow, magenta);
+        System.out.printf(RECORD, name2, age2, white, darkYellow, magenta);
+        System.out.printf(DASHLINE, darkYellow);
         //System.out.printf();
 
         
