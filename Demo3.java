ac55d7ea16a28f3ec00d93dcffd5210fc56eaff8
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-07-30 15:37:08 +0000

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,12 @@
+public class Demo3 {
+    public static void main(String[] args) {
+
+        int teleNumberSet1 = 123;
+        int teleNumberSet2 = 456;
+        int teleNumberSet3 = 7890;
+        
+        System.out.printf("Telephone_Number = \033[0;45m(%d)\033[0m %d-%d\n",teleNumberSet1,teleNumberSet2,teleNumberSet3);
+        
+    }
+    
+}
