14ef1748837be98f2e34a512f0639c422e03a8b6
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-07-30 18:33:08 +0000

    Complete Task 6-A

---
+++ Demo6Task_a.java
@@ -0,0 +1,11 @@
+public class Demo6Task_a {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        String str = "0%";
+                
+        System.out.printf("\033[41m[% 7d\033[42m%-7s]\033[0m\n", num1,str);
+        
+    }
+    
+}
