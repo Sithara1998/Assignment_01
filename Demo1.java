7fb3d2eae63001fb455e65448a23deb9d05af933
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-07-30 14:52:23 +0000

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,7 @@
+public class Demo1{
+    public static void main(String[] args) {
+        double accountBalance = 123456.7890 ;
+        
+        System.out.printf("Account_Balance = $%,.2f\n",accountBalance);
+    }
+}
